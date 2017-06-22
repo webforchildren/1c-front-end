1d444280830ee180910f08b60889caccfdf4d333
webforchildren <lettersforirinka@gmail.com>
2017-06-22 17:27:10 +0700

    lesson1

---
+++ example/script.js
@@ -0,0 +1,3 @@
+var canvas = document.getElementById("sandbox"),
+    context = canvas.getContext("2d");
+context.fillRect(50, 25, 200, 250);
---
+++ example/оформление.js
@@ -0,0 +1,13 @@
+var canvas = document.getElementById("sandbox"),
+    context = canvas.getContext("2d"),
+    square, circle;
+square = new Path2D();
+square.moveTo(50,50);
+square.lineTo(250,50);
+square.lineTo(250,250);
+square.lineTo(50,250);
+square.lineTo(50,50);
+context.stroke(square);
+circle = new Path2D();
+circle.arc(150,150,100,0,2 * Math.PI);
+context.fill(circle);
